dc3f369c2bb022ec4faf817388d2170946619109
Jerrod Mathis <jerrodmathis@proton.me>
2025-04-04 20:03:48 -0400

    1.5 added REPL

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
-package main 
+package main
+
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-  print("hello, world!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
-
