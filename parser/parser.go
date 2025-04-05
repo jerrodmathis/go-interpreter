75528c90d67cf566d1534e4012cf8b7f2a957b8e
Jerrod Mathis <jerrodmathis@proton.me>
2025-04-05 12:19:33 -0400

    parsing return statements

--- parser/parser.go
+++ parser/parser.go
@@ -60,6 +60,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.currToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -79,6 +81,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// TODO: We're skipping the expression until we
+	// encounter a semicolon
+	for !p.currTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.currToken}
+
+	p.nextToken()
+
+	// TODO: We're skipping the expressions until we
 	// encounter a semicolon
 	for !p.currTokenIs(token.SEMICOLON) {
 		p.nextToken()
